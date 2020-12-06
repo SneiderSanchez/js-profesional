2644ffd6695395f1e5d6e9fe17301936d823e52b
SneiderSanchez <sneiderandres09@hotmail.com>
2020-12-05 20:18:01 -0500

    Adjustments on MediaPlayer and folders structure

---
+++ assets/index.js
@@ -0,0 +1,24 @@
+const video = document.querySelector("video");
+const button = document.querySelector("button");
+
+function MediaPlayer(config) {
+  this.media = config.el;
+}
+
+MediaPlayer.prototype.play = function () {
+  this.media.play();
+};
+
+MediaPlayer.prototype.pause = function () {
+  this.media.pause();
+};
+
+MediaPlayer.prototype.tooglePlay = function () {
+  if (this.media.paused) {
+    player.play();
+  } else {
+    player.pause();
+  }};
+
+const player = new MediaPlayer({ el: video });
+button.onclick = () => player.tooglePlay();
