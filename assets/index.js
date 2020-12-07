d3b838e3aa597bde6e9231e0a3f27db1018a4a8d
SneiderSanchez <sneiderandres09@hotmail.com>
2020-12-07 16:05:25 -0500

    this

--- assets/index.js
+++ assets/index.js
@@ -4,6 +4,8 @@ const video = document.querySelector("video");
 const tooglePlayButton = document.getElementById('tooglePlay')
 const toogleMuteButton = document.getElementById('toogleMute')
 
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()]});
+const player = new MediaPlayer({ el: video, plugins: [
+  /* new AutoPlay() */
+]});
 tooglePlayButton.onclick = () => player.tooglePlay();
 toogleMuteButton.onclick = () => player.toogleMute();
