10d6848ce4f89f2cb5f9fa0cfdca53426dce4085
SneiderSanchez <sneiderandres09@hotmail.com>
2021-01-29 18:58:44 -0500

    service workers

--- assets/index.js
+++ assets/index.js
@@ -1,10 +1,19 @@
 import MediaPlayer from "./MediaPlayer.js";
-import AutoPlay from "../plugins/AutoPlay.js";
-import AutoPause from "../plugins/AutoPause.js";
+import AutoPlay from "./plugins/AutoPlay.js";
+import AutoPause from "./plugins/AutoPause.js";
 const video = document.querySelector("video");
 const tooglePlayButton = document.getElementById("tooglePlay");
 const toogleMuteButton = document.getElementById("toogleMute");
 
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay(), new AutoPause()] });
+const player = new MediaPlayer({
+  el: video,
+  plugins: [new AutoPlay(), new AutoPause()],
+});
 tooglePlayButton.onclick = () => player.tooglePlay();
 toogleMuteButton.onclick = () => player.toogleMute();
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("../sw.js").catch((error) => {
+    console.log(error);
+  });
+}
