c8036ff2c0a31f44bbee8e3274b3a1be48a74a95
SneiderSanchez <sneiderandres09@hotmail.com>
2021-01-29 18:11:15 -0500

    HandlerIntersection

--- assets/index.js
+++ assets/index.js
@@ -1,9 +1,10 @@
 import MediaPlayer from "./MediaPlayer.js";
 import AutoPlay from "../plugins/AutoPlay.js";
+import AutoPause from "../plugins/AutoPause.js";
 const video = document.querySelector("video");
 const tooglePlayButton = document.getElementById("tooglePlay");
 const toogleMuteButton = document.getElementById("toogleMute");
 
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlay(), new AutoPause()] });
 tooglePlayButton.onclick = () => player.tooglePlay();
 toogleMuteButton.onclick = () => player.toogleMute();
