b3c5023cd54b0a1eb15a3e0640f11268adc7b447
SneiderSanchez <sneiderandres09@hotmail.com>
2021-02-01 16:50:39 -0500

    Basic types

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -1,7 +1,44 @@
-console.log("hello, Typescript");
+// Boolean
+let muted: boolean = true;
+muted = false;
 
-function add(a: number, b: number) {
-  return a + b;
+// numbers
+const age = 6;
+let numerador: number = 42;
+let denominador: number = age;
+let resultado = numerador / denominador;
+
+// string
+let nombre: string = "Sneider";
+let saludo = `My name is ${nombre}`;
+
+// Arrays
+let people: string[] = [];
+people = ["Anya", "Kiara", "Elsa"];
+// people.push(9);
+
+let peopleAndNumbers: Array<string | number> = [];
+peopleAndNumbers.push("Lily");
+peopleAndNumbers.push(123);
+
+// Enum
+
+enum Color {
+  red = "Red",
+  green = "Green",
+  blue = "Blue",
+  yellow = "Yellow",
 }
 
-const sum = add(2, 4);
+const favoriteColor: Color = Color.yellow;
+
+console.log(`My favorite color is  ${favoriteColor}`);
+
+// Any
+
+let comodin: any = "Joker";
+
+comodin = { type: "hola" };
+
+// Object
+let someObject: object = {};
