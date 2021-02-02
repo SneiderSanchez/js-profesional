657dbd1ca4588bb2723a79effa7c267f41419d6a
SneiderSanchez <sneiderandres09@hotmail.com>
2021-02-02 18:38:46 -0500

    Interfaces

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -65,3 +65,29 @@ function fullName(firstName: string, lastName: string = "Sanchez"): string {
 const sneider = fullName("Sneider");
 
 console.log(sneider);
+
+// interfaces
+interface Rectangulo {
+  ancho: number;
+  alto: number;
+  color?: Color;
+}
+
+let rect: Rectangulo = {
+  ancho: 4,
+  alto: 6,
+  color: Color.red,
+};
+
+function area(r: Rectangulo) {
+  return r.alto * r.ancho;
+}
+
+const areaRect = area(rect);
+
+console.log(areaRect);
+
+rect.toString = function () {
+  return this.color ? `Un rectangulo ${this.color}` : `Un rectangulo`;
+};
+console.log(rect.toString());
