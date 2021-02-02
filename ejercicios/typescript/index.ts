203e33f435cbb154401f555486fe8e1c94959758
SneiderSanchez <sneiderandres09@hotmail.com>
2021-02-02 18:28:42 -0500

    Ts Functions

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -42,3 +42,26 @@ comodin = { type: "hola" };
 
 // Object
 let someObject: object = {};
+
+// Functions
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+const sum = add(4, 2);
+function createAdder(a: number): (number) => number {
+  return function (b: number) {
+    return b + a;
+  };
+}
+
+const addFour = createAdder(4);
+const fourPlusSix = addFour(6);
+
+function fullName(firstName: string, lastName: string = "Sanchez"): string {
+  return `${firstName} ${lastName}`;
+}
+
+const sneider = fullName("Sneider");
+
+console.log(sneider);
