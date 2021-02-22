126b038ccb8afd2082b6f9858230d2a7dc0f398f
SneiderSanchez <sneiderandres09@hotmail.com>
2021-02-22 18:33:53 -0500

    feat: Publishing node_modules library and implementing it on code

--- webside/assets/index.ts
+++ webside/assets/index.ts
@@ -1,7 +1,7 @@
-import MediaPlayer from "./MediaPlayer";
-import AutoPlay from "./plugins/AutoPlay";
-import AutoPause from "./plugins/AutoPause";
-import Ads from "./plugins/Ads";
+import MediaPlayer from "@sneidersanchez/platzimediaplayer/src/MediaPlayer";
+import AutoPlay from "@sneidersanchez/platzimediaplayer/src/plugins/AutoPlay";
+import AutoPause from "@sneidersanchez/platzimediaplayer/src/plugins/AutoPause";
+import Ads from "@sneidersanchez/platzimediaplayer/src/plugins/Ads";
 
 const video = document.querySelector("video");
 const tooglePlayButton: HTMLElement = document.getElementById("tooglePlay");
